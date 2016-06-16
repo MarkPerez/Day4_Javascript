fb9a936847e1540fa9df073c353b3d7b1b33365d
MarkPerez <markperez88@hotmail.co.uk>
2016-06-16 11:45:27 +0100

    Objects exercise 1

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+
+var person = {
+    name:"Mark",
+    age:"27",
+    eyeColour:"Brown"
+};
+
+
+document.getElementById("demo").innerHTML = "Name: " + person.name + "<br>" + "Age: " + person.age + "<br>" + "Eye colour: " + person.eyeColour;
+
