c29ea45efc96817d71ecbeff186c8c4cbc04f8a8
MarkPerez <markperez88@hotmail.co.uk>
2016-06-16 12:24:24 +0100

    Objects exercise 2

--- Javascript.js
+++ Javascript.js
@@ -3,18 +3,14 @@ function car(make, model, colour){
     this.make = make;
     this.model = model;
     this.colour = colour;
-    //create property with method
-    this.price = function(cost, discount){
-        return cost - discount;
+
     };
 
-};
+
 
 //create new object
 var newCar = new car("Nissan", "Micra", "Green");
 
-//acces the method
-var carPrice = newCar.price(5000, 200);
 
-document.getElementById("demo").innerHTML = "Make: " + newCar.make + "<br>" + "Model: " + newCar.model + "<br>" + "Colour: " + newCar.colour + "<br>" + "Price: " + carPrice;
+document.getElementById("demo").innerHTML = "Make: " + newCar.make + "<br>" + "Model: " + newCar.model + "<br>" + "Colour: " + newCar.colour;
 
